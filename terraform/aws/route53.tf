50ee66d9db233e13aae5825c903c8e6d06f7022e
Tomas Ferrari <tferrari.92@gmail.com>
2023-12-19 03:00:05 +0100

    fixing UAT OPA

--- terraform/aws/route53.tf
+++ terraform/aws/route53.tf
@@ -9,3 +9,16 @@ output "aws_route53_zone_id" {
 output "aws_route53_zone_name_servers" {
   value = aws_route53_zone.main.name_servers
 }
+
+
+resource "aws_route53_zone" "sub" {
+  name = "nginx.tferrari.com"
+}
+
+output "aws_route53_zone_id_sub" {
+  value = aws_route53_zone.sub.zone_id
+}
+
+output "aws_route53_zone_name_servers_sub" {
+  value = aws_route53_zone.sub.name_servers
+}
