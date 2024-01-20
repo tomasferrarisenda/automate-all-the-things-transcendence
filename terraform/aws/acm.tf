b9ba022b424dd54e35ce8ea99ad8166e98660647
Tomas Ferrari <tferrari.92@gmail.com>
2024-01-20 01:14:27 +0100

    add acm and validations

--- terraform/aws/acm.tf
+++ terraform/aws/acm.tf
@@ -1,3 +1,4 @@
+# Certificates
 resource "aws_acm_certificate" "argocd" {
   domain_name       = "argocd.${var.domain}"
   validation_method = "DNS"
@@ -41,4 +42,32 @@ resource "aws_acm_certificate" "kiali" {
   lifecycle {
     create_before_destroy = true
   }
+}
+
+
+# Validations
+resource "aws_acm_certificate_validation" "argocd" {
+  certificate_arn         = aws_acm_certificate.argocd.arn
+  validation_record_fqdns = ["argocd.${var.domain}"]
+#   validation_record_fqdns = [for record in aws_route53_record.example : record.fqdn]
+}
+
+resource "aws_acm_certificate_validation" "grafana" {
+  certificate_arn         = aws_acm_certificate.grafana.arn
+  validation_record_fqdns = ["argocd.${var.domain}"]
+}
+
+resource "aws_acm_certificate_validation" "harbor" {
+  certificate_arn         = aws_acm_certificate.harbor.arn
+  validation_record_fqdns = ["argocd.${var.domain}"]
+}
+
+resource "aws_acm_certificate_validation" "jaeger" {
+  certificate_arn         = aws_acm_certificate.jaeger.arn
+  validation_record_fqdns = ["argocd.${var.domain}"]
+}
+
+resource "aws_acm_certificate_validation" "kiali" {
+  certificate_arn         = aws_acm_certificate.kiali.arn
+  validation_record_fqdns = ["argocd.${var.domain}"]
 }
